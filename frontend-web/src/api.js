17419752d577f0bb252f8e127e23ff2323597245
Ryan John Mathew <75768806+ryanzone@users.noreply.github.com>
2026-04-17 23:39:21 +0530

    Update api.js

--- frontend-web/src/api.js
+++ frontend-web/src/api.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://localhost:5000";
-export const ML_BASE = "http://localhost:5005";
+export const API_BASE = "https://helion-ai.onrender.com";
+export const ML_BASE = "https://helion-ai-ml.onrender.com";
